d09e6eaf72f17aef5db6deb04760b4ee3faa8ba2
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-10 22:20:10 -0300

    feat :sparkles: Adicionando loading ao layout

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,9 +1,24 @@
 import "@/styles/global.css";
 
-import { Slot } from 'expo-router';
-import { View, StatusBar} from 'react-native';
+import { Slot } from "expo-router";
+import { View, StatusBar} from "react-native";
+
+
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from "@expo-google-fonts/inter";
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Inter_500Medium,
+        Inter_400Regular,
+        Inter_600SemiBold,
+    });
+
+    if (!fontsLoaded) {
+      return <Loading />
+    }
+
   return (
     <View className="flex-1 bg-zinc-950">
       <StatusBar 
