4041865768cfe1d3a549c8eee1c41a391566b9d1
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-11 00:19:17 -0300

    feat :sparkles: adicionando o forms no index

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,14 @@
 import { Input } from "@/components/input";
 import { View, Text, Image } from "react-native";
+import { MapPin, Calendar as IconCalendar } from "lucide-react-native";
+
+import { colors } from "@/styles/colors"
+import { Button } from "@/components/button";
+
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 px-4 items-center justify-center">
       <Image 
         source={require("@/assets/logo.png")} 
         className="h-8" 
@@ -14,10 +19,20 @@ export default function Index() {
       >
         Convide seus amigos e planeje sua{"\n"} próxima viagem!
       </Text>
-      <View>
+      <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
         <Input>
-          <Input.Field placeholder="Para onde vamos?"/>
-        </Input>
+          <MapPin color={ colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Para onde ?" />        
+       </Input>
+       <Input>
+          <IconCalendar color={ colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Quando ?" />        
+       </Input>
+       <View className="border-b py-3 border-zinc-800">
+        <Button>
+          <Button.Title className="text-white"> Alterar Local/Data</Button.Title>
+        </Button>
+       </View>
       </View>
     </View>
   );
