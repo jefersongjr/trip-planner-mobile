1ff4b3949b4832c9e3d52647930b4dbaadba53d9
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-10 21:53:38 -0300

    feat :sparkles: adicionando texto ao app

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native"
 export default function Index() {
     return (
         <View className="flex-1 items-center justify-center">
-            <Text>Olá Mundo!</Text>
+            <Text>Convide seus amigos e planeje sua proxima viagem!</Text>
         </View>
-    )
+    );
 }
