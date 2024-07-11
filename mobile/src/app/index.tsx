dcd97124f253cfdd48cc6cc7b9774ee2b5cb0b62
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-10 23:00:30 -0300

    feat :sparkles: adicionando elementos no index

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from "react-native"
+import { Input } from "@/components/input";
+import { View, Text, Image } from "react-native";
 
 export default function Index() {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="text-zinc-400">Convide seus amigos e planeje sua proxima viagem!</Text>
-        </View>
-    );
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Image 
+        source={require("@/assets/logo.png")} 
+        className="h-8" 
+        resizeMode="contain"
+      />
+      <Text 
+        className="text-zinc-400 font-regular text-center text-lg mt-3"
+      >
+        Convide seus amigos e planeje sua{"\n"} próxima viagem!
+      </Text>
+      <View>
+        <Input>
+          <Input.Field />
+        </Input>
+      </View>
+    </View>
+  );
 }
