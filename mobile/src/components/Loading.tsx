1d3cc692635be8f8838dfb59bb6132c064886815
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-10 22:02:13 -0300

    style :gem: estilizando loading

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -2,7 +2,8 @@ import { ActivityIndicator } from "react-native";
 
 export default function Loading(){
   return (
-  <ActivityIndicator 
-    />
+  <ActivityIndicator
+    className="flex-1 bg-zinc-950 flex items-center justify-center text-lime-300"
+   />
   )
 }
