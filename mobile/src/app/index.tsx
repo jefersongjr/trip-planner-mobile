57583af64f3d970136526e42e118aee29d49fe02
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-10 22:20:40 -0300

    style :gem: mundando cor do texto

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native"
 export default function Index() {
     return (
         <View className="flex-1 items-center justify-center">
-            <Text>Convide seus amigos e planeje sua proxima viagem!</Text>
+            <Text className="text-zinc-400">Convide seus amigos e planeje sua proxima viagem!</Text>
         </View>
     );
 }
