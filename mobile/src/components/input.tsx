df8b6869378c319998d4ad2c55413582d2645ad4
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-11 00:18:19 -0300

    feat :sparkles: criando inputs

--- mobile/src/components/input.tsx
+++ mobile/src/components/input.tsx
@@ -1,27 +1,58 @@
 import { ReactNode } from "react";
-import { TextInput, View } from "react-native";
+import {
+    Platform,
+  TextInput,
+  TextInputProps,
+  View,
+  ViewProps,
+} from "react-native";
 import clsx from "clsx";
 
-type Variants = "primary" | "secondary" | "tertiary";
+import { colors } from "@/styles/colors"
 
-type InputProps = {
-  children: ReactNode,
+type Variants = "primary" | "secondary" | "tertiary"
+
+type InputProps = ViewProps & {
+  children: ReactNode
   variant?: Variants
-};
-
-function Input({ children, variant = "primary" }: InputProps) {
-  return <View className={clsx(
-    "w-full h-16 flex-row items-center gap-2",
-    { "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary" },
-    { "bg-zinc-950": variant === "secondary" },
-    { "bg-zinc-900": variant === "tertiary" },
-  )}>{children}</View>;
 }
 
-function Field() {
-  return <TextInput className="flex-1 text-zinc-100 text-lg font-regular"/>;
+function Input({
+  children,
+  variant = "primary",
+  className,
+  ...rest
+}: InputProps) {
+  return (
+    <View
+      className={clsx(
+        "min-h-16 max-h-16 flex-row items-center gap-2",
+        {
+          "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary",
+          "bg-zinc-950": variant === "secondary",
+          "bg-zinc-900": variant === "tertiary",
+        },
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </View>
+  )
 }
 
-Input.Field = Field;
+function Field({ ...rest }: TextInputProps) {
+  return (
+    <TextInput
+    className="flex-1 text-zinc-100 text-lg font-regular"
+    placeholderTextColor={colors.zinc[400]}
+    cursorColor={colors.zinc[100]}
+    selectionColor={Platform.OS === "ios" ? colors.zinc[100] : undefined} //para mudar o cursor no ios
+    {...rest}
+  />
+  )
+}
 
-export { Input };
+Input.Field = Field
+
+export { Input }
